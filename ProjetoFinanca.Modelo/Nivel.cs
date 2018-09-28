a3940f805351e6ea96cb23c0f3737d4cff44f3fe
leobogv <leobo@hotmail.com>
2018-09-28 17:10:42 -0300

    Nivel e usuario (Model)

---
+++ ProjetoFinanca.Modelo/Nivel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinanca.Modelo
+{
+    public class Nivel
+    {
+        public int NivelId { get; set; }
+        public string Nome { get; set; }
+        public virtual ICollection<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ ProjetoFinanca.Modelo/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Activation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinanca.Modelo
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public virtual Nivel Nivel { get; set; }
+    }
+}
